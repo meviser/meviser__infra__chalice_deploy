d877773a0e8cd0dbd444beacc8056ac2b1c3ebf2
Rafal <rafal.szota@adevinta.com>
2022-09-22 15:49:11 +0200

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -l
+
+if [ ! -z "$WORKING_DIRECTORY" ]
+then
+      cd "$WORKING_DIRECTORY"
+fi
+
+if [ ! -z "$REQUIREMENTS_FILE" ]
+then
+      pip install -r "$REQUIREMENTS_FILE" -t ./vendor
+fi
+
+if ! [ -z "$PROJECT_DIR" ]
+then
+      projectDirSubCommand="--project-dir $PROJECT_DIR"
+fi
+
+if ! [ -z "$STAGE" ]
+then
+      stageSubCommand="--stage $STAGE"
+fi
+
+command="chalice $projectDirSubCommand $OPERATION $stageSubCommand"
+
+eval $command
